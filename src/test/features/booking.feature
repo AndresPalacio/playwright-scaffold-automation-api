e43b25185f98e3340b05e87963ae5abffdd22e5b
SebastianRG2 <sebastianramirez.g@outlook.com>
2025-02-18 22:39:39 -0500

    Create booking api

--- src/test/features/booking.feature
+++ src/test/features/booking.feature
@@ -6,4 +6,17 @@ Feature: Get booking list
 
 @api
   Scenario: Create new booking should be success
-    When user get booking list
+   When user create new book with payload
+      """
+      {
+        "firstname" : "Jim",
+        "lastname" : "Brown",
+        "totalprice" : 111,
+        "depositpaid" : true,
+        "bookingdates" : {
+            "checkin" : "2018-01-01",
+            "checkout" : "2019-01-01"
+        },
+        "additionalneeds" : "Breakfast"
+      }
+      """
--- src/test/steps/booking.ts
+++ src/test/steps/booking.ts
@@ -26,20 +26,24 @@ When('user get booking list', async function () {
 });
 
 
-When('create new book', async function () {
+When('user create new book with payload', async function (docString) {
     /* Test Flow
         1. Hit API endpoint
         2. Verify API status code
         3. Verify JSON Response
         4. Verify JSON Schema
     */
-    console.log("This will make GET call to https://restful-booker.herokuapp.com/booking with no authentication");
+    console.log("This will make POST call to https://restful-booker.herokuapp.com/booking with no authentication");
 
     // Inicializa el contexto de solicitud
     const apiContext = await request.newContext();
     const apiHelper = new ApiHelper(apiContext);
 
-    const responseMsg = await apiHelper.invokeGetApi("/booking");
+    // Parse the payload from the feature file
+    const payload = JSON.parse(docString);
+
+
+    const responseMsg = await apiHelper.invokePostApi("/booking", payload , 200);
     console.log(JSON.stringify(responseMsg));
     for (let index = 0; index < responseMsg.length; index++) {
         console.log(`BookingId : ${responseMsg[index].bookingid}`);
